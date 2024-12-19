6c0b1a0d4474e226213d3751124f6808f0ceeb37
Einzelganger <_@.com>
2024-12-19 11:31:59 +0300

    '++поименипакета'

--- packagename.py
+++ packagename.py
@@ -8,7 +8,7 @@ def extract_gz_file(archive_path):
     """
     Если это текстовый файл, просто читаем его.
     """
-    with open(archive_path, 'r', encoding='utf-8') as f:
+    with gzip.open(archive_path, 'rt', encoding='utf-8') as f:
         content = f.read()
     return content
 
@@ -30,23 +30,27 @@ def parse_dependencies(content):
                 package_name = line.split(":", 1)[1].strip()
             elif line.startswith("Depends:"):
                 depends = line.split(":", 1)[1].strip()
-                package_deps = [dep.strip() for dep in depends.split(",")]
+                package_deps = [dep.split()[0].strip() for dep in depends.split(",")]
         
-        if package_name and package_deps:
+        if package_name:
             dependencies[package_name] = package_deps
     
     return dependencies
 
-def build_dependency_graph(dependencies, max_dependencies=20):
+def build_dependency_graph(dependencies, root_package):
+    """
+    Строит граф зависимостей для заданного пакета и его зависимостей.
+    """
+    G = nx.DiGraph()
+    
+    def add_dependencies(package):
+        if package in dependencies:
+            for dep in dependencies[package]:
+                G.add_edge(package, dep)
+                if dep not in G:
+                    add_dependencies(dep)
 
-    G = nx.DiGraph()  # Направленный граф
-    count = 0
-    for package, deps in dependencies.items():
-        if count >= max_dependencies:  
-            break
-        for dep in deps:
-            G.add_edge(package, dep)
-        count += 1
+    add_dependencies(root_package)
     return G
 
 def save_graph_as_png(G, output_file):
@@ -65,21 +69,24 @@ def save_graph_as_png(G, output_file):
     except subprocess.CalledProcessError:
         print(f"Ошибка при рендеринге графа в {output_file}. Убедитесь, что Graphviz установлен.")
 
-def main(archive_path, output_file):
+def main(archive_path, output_file, root_package):
     content = extract_gz_file(archive_path)
     
     dependencies = parse_dependencies(content)
     
-    G = build_dependency_graph(dependencies, max_dependencies=20)
+    if root_package not in dependencies:
+        print(f"Пакет {root_package} не найден в архиве.")
+        sys.exit(1)
+    
+    G = build_dependency_graph(dependencies, root_package)
     
     save_graph_as_png(G, output_file)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Использование: python visualizer.py <путь_к_Packages.gz> <выходной_файл.png>")
+    if len(sys.argv) != 4:
+        print("Использование: python visualizer.py <путь_к_Packages.gz> <выходной_файл.png> <имя_пакета>")
         sys.exit(1)
     
     archive_path = sys.argv[1]
     output_file = sys.argv[2]
-    
-    main(archive_path, output_file)
+    root_package = sys.argv[3]
